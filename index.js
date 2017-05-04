bbe45f8e8b60f236be5ab621559a6efdfc1fc82d
Viktor <vikkes@gmail.com>
2017-05-04 14:14:26 +0000

    bot dialog is ready

--- index.js
+++ index.js
@@ -16,20 +16,18 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/tribotum/answer', function(req, res) {
-    //console.log(req.body);
     res.setHeader('Access-Control-Allow-Origin', '*');
     var speech = req.body.answer ? req.body.answer : "none";
     var qId = req.body.qId ? req.body.qId : 0;
     var answerExpected = obj[qId]["condition"]["answer"];
     var qIdNext;
     
-    console.log(qId +" : " + speech +" : "+ answerExpected);
+    
 
     if (answerExpected == speech) {
         qIdNext = obj[qId]["condition"]["follower"];
-        
         speech = obj[qIdNext];
-        console.log(qIdNext + " : " +speech);
+        
     }
     else if (answerExpected == "text") {
         qIdNext = obj[qId]["condition"]["follower"];
@@ -58,7 +56,6 @@ restService.get('/tribotum/newuser', function(req, res) {
 restService.get('/tribotum/question', function(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     var num = req.query.num ? req.query.num : 1
-    //console.log(num)
     fs.readFile('/home/ubuntu/workspace/bot/data/questions.json', 'utf8', function(err, data) {
         if (err) {
             return res.json({
@@ -67,10 +64,8 @@ restService.get('/tribotum/question', function(req, res) {
             });
         }
 
-        obj = JSON.parse(data)
-        //console.log(data);
+        obj = JSON.parse(data);
         var tempObj = obj[num];
-        //console.log(tempObj);
         return res.json({
             qId: num,
             content: tempObj
@@ -82,4 +77,4 @@ restService.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
 });
 
-//TODO: codnitional request
+
